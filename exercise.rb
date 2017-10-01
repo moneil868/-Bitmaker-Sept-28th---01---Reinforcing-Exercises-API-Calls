2b8f269d4870ae878c4bbd9c2794d04d9e9b858a
Marlon O'Neil <hello@marlononeil.com>
2017-09-30 22:39:21 -0400

    Completed exercise

--- exercise.rb
+++ exercise.rb
@@ -3,4 +3,12 @@ require 'HTTParty'
 
 # Try making a request to this API's endpoint.
 response = HTTParty.get('http://setgetgo.com/randomword/get.php')
-puts response
+
+# Use this API to generate a list of 10 random words.
+def api_calls
+  10.times do
+    puts HTTParty.get('http://setgetgo.com/randomword/get.php')
+  end
+end
+
+api_calls
